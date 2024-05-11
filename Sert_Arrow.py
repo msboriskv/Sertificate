83289014c1e55e0af8e02dc09c3b9329204fc5b4
BorisKV <boris@karnaval.su>
2024-05-11 09:55:42 +0300

    Email

--- Sert_Arrow.py
+++ Sert_Arrow.py
@@ -11,45 +11,76 @@ from openpyxl.utils import get_column_letter
 from openpyxl.utils.cell import rows_from_range
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font, Fill, PatternFill, NamedStyle
 from fpdf import FPDF
+import smtplib
+import os
 
-workBook = load_workbook(filename = 'Anckett.xlsx')
-sheets = workBook.active
+# workBook = load_workbook(filename = 'Anckett.xlsx')
+# sheets = workBook.active
 
-full_name = ''
-code = ''
-for collum in range(0, sheets.max_collum):
-    if sheets[0].value == NULL:
-        break
-    for row in range(1, sheets.max_row):
-        fist_name = sheets[row][0].value
-        name = sheets[row][1].value
-        second_name = sheets[row][2].value
-        code = sheets[row][3].value
-        mail = sheets[row][4].value
+# baze_point = 340
+
+# full_name = ''
+# code = ''
+
+# for row in range(1, sheets.max_row + 1):
     
-        full_name = fist_name+' '+name+second_name
+#     fist_name = sheets[row][0].value
+#     if fist_name == None:
+#         fist_name = ''
+        
+#     name = sheets[row][1].value
+#     if name == None:
+#         name = ''
+        
+#     second_name = sheets[row][2].value
+#     if second_name == None:
+#         second_name = ''
+        
+#     code = sheets[row][3].value
+#     if code == None:
+#         code = ''
+        
+#     mail = sheets[row][4].value
+#     if mail == None:
+#         mail = ''
     
-        print(row, full_name)
+#     full_name = fist_name + ' ' + name + ' ' + second_name
+    
+#     print(row, full_name, code, mail)
 
-pdf = FPDF('P', 'mm', 'A4')
+#     pdf = FPDF('P', 'mm', 'A4')
 
-pdf.add_font("Sans", style="", fname="Noto Sans/NotoSans-Regular.ttf", uni=True)
-pdf.add_font("Sans", style="B", fname="Noto Sans/NotoSans-Bold.ttf", uni=True)
-pdf.add_font("Sans", style="I", fname="Noto Sans/NotoSans-Italic.ttf", uni=True)
-pdf.add_font("Sans", style="BI", fname="Noto Sans/NotoSans-BoldItalic.ttf", uni=True)
+#     pdf.add_font("Sans", style="", fname="Noto Sans/NotoSans-Regular.ttf", uni=True)
+#     pdf.add_font("Sans", style="B", fname="Noto Sans/NotoSans-Bold.ttf", uni=True)
+#     pdf.add_font("Sans", style="I", fname="Noto Sans/NotoSans-Italic.ttf", uni=True)
+#     pdf.add_font("Sans", style="BI", fname="Noto Sans/NotoSans-BoldItalic.ttf", uni=True)
 
-pdf.add_page()
+#     pdf.add_page()
 
-pdf.image("555.png", x=5, y=5, w=200)
+#     pdf.image("555.png", x=5, y=5, w=200)
 
-pdf.set_font("Sans", "B", 20)
-pdf.cell(0, 340, 'Получил ' + full_name, new_x="LMARGIN", align='C')
+#     pdf.set_font("Sans", "B", 20)
+#     pdf.cell(0, baze_point, 'Получил', new_x="LMARGIN", align='C')
+#     pdf.cell(0, baze_point + 20, full_name, new_x="LMARGIN", align='C')
 
-pdf.set_font("Sans", "", 16)
-pdf.cell(0, 380, 'за участие в XII международном форуме', new_x="LMARGIN", align='C')
-pdf.cell(0, 395, '"ОБРАЗОВАНИЕ: РЕАЛИИ И ПЕРСПЕКТИВЫ"', new_x="LMARGIN", align='C')
+#     pdf.set_font("Sans", "", 16)
+#     pdf.cell(0, baze_point + 60, 'за участие в XII международном форуме', new_x="LMARGIN", align='C')
+#     pdf.cell(0, baze_point + 75, '"ОБРАЗОВАНИЕ: РЕАЛИИ И ПЕРСПЕКТИВЫ"', new_x="LMARGIN", align='C')
 
-pdf.set_font("Sans", "", 14)
-pdf.cell(0, 500, code, new_x="LMARGIN", new_y="NEXT", align='C')
+#     pdf.set_font("Sans", "", 14)
+#     pdf.cell(0, 500, code, new_x="LMARGIN", new_y="NEXT", align='C')
+    
+#     pdf.set_display_mode(zoom='fullpage', layout='continuous')
 
-pdf.output('Forum.pdf')
+#     pdf.output(full_name + '_' + code + '.pdf')
+
+send = "msboriskv@gmail.com"
+pas = "aixd svcr amnq fsvj"
+to = "boris@karnaval.su"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+
+server.login(send, pas)
+
+server.sendmail(send, to, "Hello fghdfgjhfsgjfhsdgjhf")
