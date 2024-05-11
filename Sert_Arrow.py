efcdf615f2109e59504de44c1cdfc42a9ef5cb3e
BorisKV <boris@karnaval.su>
2024-05-11 10:23:02 +0300

    234

--- Sert_Arrow.py
+++ Sert_Arrow.py
@@ -14,73 +14,72 @@ from fpdf import FPDF
 import smtplib
 import os
 
-# workBook = load_workbook(filename = 'Anckett.xlsx')
-# sheets = workBook.active
+workBook = load_workbook(filename = 'Anckett.xlsx')
+sheets = workBook.active
 
-# baze_point = 340
+baze_point = 340
 
-# full_name = ''
-# code = ''
+full_name = ''
+code = ''
 
-# for row in range(1, sheets.max_row + 1):
+for row in range(1, sheets.max_row + 1):
     
-#     fist_name = sheets[row][0].value
-#     if fist_name == None:
-#         fist_name = ''
+    fist_name = sheets[row][0].value
+    if fist_name == None:
+        fist_name = ''
         
-#     name = sheets[row][1].value
-#     if name == None:
-#         name = ''
+    name = sheets[row][1].value
+    if name == None:
+        name = ''
         
-#     second_name = sheets[row][2].value
-#     if second_name == None:
-#         second_name = ''
+    second_name = sheets[row][2].value
+    if second_name == None:
+        second_name = ''
         
-#     code = sheets[row][3].value
-#     if code == None:
-#         code = ''
+    code = sheets[row][3].value
+    if code == None:
+        code = ''
         
-#     mail = sheets[row][4].value
-#     if mail == None:
-#         mail = ''
+    mail = sheets[row][4].value
+    if mail == None:
+        mail = ''
     
-#     full_name = fist_name + ' ' + name + ' ' + second_name
+    full_name = fist_name + ' ' + name + ' ' + second_name
     
-#     print(row, full_name, code, mail)
+    print(row, full_name, code, mail)
 
-#     pdf = FPDF('P', 'mm', 'A4')
+    pdf = FPDF('P', 'mm', 'A4')
 
-#     pdf.add_font("Sans", style="", fname="Noto Sans/NotoSans-Regular.ttf", uni=True)
-#     pdf.add_font("Sans", style="B", fname="Noto Sans/NotoSans-Bold.ttf", uni=True)
-#     pdf.add_font("Sans", style="I", fname="Noto Sans/NotoSans-Italic.ttf", uni=True)
-#     pdf.add_font("Sans", style="BI", fname="Noto Sans/NotoSans-BoldItalic.ttf", uni=True)
+    pdf.add_font("Sans", style="", fname="Noto Sans/NotoSans-Regular.ttf", uni=True)
+    pdf.add_font("Sans", style="B", fname="Noto Sans/NotoSans-Bold.ttf", uni=True)
+    pdf.add_font("Sans", style="I", fname="Noto Sans/NotoSans-Italic.ttf", uni=True)
+    pdf.add_font("Sans", style="BI", fname="Noto Sans/NotoSans-BoldItalic.ttf", uni=True)
 
-#     pdf.add_page()
+    pdf.add_page()
 
-#     pdf.image("555.png", x=5, y=5, w=200)
+    pdf.image("555.png", x=5, y=5, w=200)
 
-#     pdf.set_font("Sans", "B", 20)
-#     pdf.cell(0, baze_point, 'Получил', new_x="LMARGIN", align='C')
-#     pdf.cell(0, baze_point + 20, full_name, new_x="LMARGIN", align='C')
+    pdf.set_font("Sans", "B", 20)
+    pdf.cell(0, baze_point, 'Получил', new_x="LMARGIN", align='C')
+    pdf.cell(0, baze_point + 20, full_name, new_x="LMARGIN", align='C')
 
-#     pdf.set_font("Sans", "", 16)
-#     pdf.cell(0, baze_point + 60, 'за участие в XII международном форуме', new_x="LMARGIN", align='C')
-#     pdf.cell(0, baze_point + 75, '"ОБРАЗОВАНИЕ: РЕАЛИИ И ПЕРСПЕКТИВЫ"', new_x="LMARGIN", align='C')
+    pdf.set_font("Sans", "", 16)
+    pdf.cell(0, baze_point + 60, 'за участие в XII международном форуме', new_x="LMARGIN", align='C')
+    pdf.cell(0, baze_point + 75, '"ОБРАЗОВАНИЕ: РЕАЛИИ И ПЕРСПЕКТИВЫ"', new_x="LMARGIN", align='C')
 
-#     pdf.set_font("Sans", "", 14)
-#     pdf.cell(0, 500, code, new_x="LMARGIN", new_y="NEXT", align='C')
+    pdf.set_font("Sans", "", 14)
+    pdf.cell(0, 500, code, new_x="LMARGIN", new_y="NEXT", align='C')
     
-#     pdf.set_display_mode(zoom='fullpage', layout='continuous')
+    pdf.set_display_mode(zoom='fullpage', layout='continuous')
 
-#     pdf.output(full_name + '_' + code + '.pdf')
+    pdf.output(full_name + '_' + code + '.pdf')
 
-send = "msboriskv@gmail.com"
+sender = "msboriskv@gmail.com"
 pas = "aixd svcr amnq fsvj"
-to = "boris@karnaval.su"
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 
-server.login(send, pas)
+server.login(sender, pas)
 
-server.sendmail(send, to, "Hello fghdfgjhfsgjfhsdgjhf")
+server.sendmail(sender, mail, "Hello fghdfgjhfsgjfhsdgjhf")
