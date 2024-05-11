6a59f18007e231887bc35b083ae83b65796da507
BorisKV <boris@karnaval.su>
2024-05-11 14:25:30 +0300

    Send_ALL

--- Sert_Arrow.py
+++ Sert_Arrow.py
@@ -23,106 +23,111 @@ from email.mime.audio import MIMEAudio
 from email.mime.application import MIMEApplication
 from email.mime.base import MIMEBase
 from dotenv import load_dotenv
+import shutil
 
-# workBook = load_workbook(filename = 'Anckett.xlsx')
-# sheets = workBook.active
+workBook = load_workbook(filename = 'Anckett.xlsx')
+sheets = workBook.active
 
 load_dotenv()
 
-# baze_point = 340
+baze_point = 340
 
-# full_name = ''
-# code = ''
+full_name = ''
+code = ''
+mail = ''
 
-# for row in range(1, sheets.max_row + 1):
+for row in range(1, sheets.max_row + 1):
     
-#     fist_name = sheets[row][0].value
-#     if fist_name == None:
-#         fist_name = ''
+    os.mkdir('To_Send')
+    
+    fist_name = sheets[row][0].value
+    if fist_name == None:
+        fist_name = ''
         
-#     name = sheets[row][1].value
-#     if name == None:
-#         name = ''
+    name = sheets[row][1].value
+    if name == None:
+        name = ''
         
-#     second_name = sheets[row][2].value
-#     if second_name == None:
-#         second_name = ''
+    second_name = sheets[row][2].value
+    if second_name == None:
+        second_name = ''
         
-#     code = sheets[row][3].value
-#     if code == None:
-#         code = ''
+    code = sheets[row][3].value
+    if code == None:
+        code = ''
         
-#     mail = sheets[row][4].value
-#     if mail == None:
-#         mail = ''
+    mail = sheets[row][4].value
+    if mail == None:
+        mail = ''
     
-#     full_name = fist_name + ' ' + name + ' ' + second_name
-#     file_name = "full_name + '_' + code + '.pdf'"
+    full_name = fist_name + ' ' + name + ' ' + second_name
+    file_name = "full_name + '_' + code + '.pdf'"
     
-#     print(row, full_name, code, mail)
+    print(row, full_name, code, mail)
 
-#     pdf = FPDF('P', 'mm', 'A4')
+    pdf = FPDF('P', 'mm', 'A4')
 
-#     pdf.add_font("Sans", style="", fname="Noto Sans/NotoSans-Regular.ttf", uni=True)
-#     pdf.add_font("Sans", style="B", fname="Noto Sans/NotoSans-Bold.ttf", uni=True)
-#     pdf.add_font("Sans", style="I", fname="Noto Sans/NotoSans-Italic.ttf", uni=True)
-#     pdf.add_font("Sans", style="BI", fname="Noto Sans/NotoSans-BoldItalic.ttf", uni=True)
+    pdf.add_font("Sans", style="", fname="Noto Sans/NotoSans-Regular.ttf", uni=True)
+    pdf.add_font("Sans", style="B", fname="Noto Sans/NotoSans-Bold.ttf", uni=True)
+    pdf.add_font("Sans", style="I", fname="Noto Sans/NotoSans-Italic.ttf", uni=True)
+    pdf.add_font("Sans", style="BI", fname="Noto Sans/NotoSans-BoldItalic.ttf", uni=True)
 
-#     pdf.add_page()
+    pdf.add_page()
 
-#     pdf.image("555.png", x=5, y=5, w=200)
+    pdf.image("555.png", x=5, y=5, w=200)
 
-#     pdf.set_font("Sans", "B", 20)
-#     pdf.cell(0, baze_point, 'Получил', new_x="LMARGIN", align='C')
-#     pdf.cell(0, baze_point + 20, full_name, new_x="LMARGIN", align='C')
+    pdf.set_font("Sans", "B", 20)
+    pdf.cell(0, baze_point, 'Получил', new_x="LMARGIN", align='C')
+    pdf.cell(0, baze_point + 20, full_name, new_x="LMARGIN", align='C')
 
-#     pdf.set_font("Sans", "", 16)
-#     pdf.cell(0, baze_point + 60, 'за участие в XII международном форуме', new_x="LMARGIN", align='C')
-#     pdf.cell(0, baze_point + 75, '"ОБРАЗОВАНИЕ: РЕАЛИИ И ПЕРСПЕКТИВЫ"', new_x="LMARGIN", align='C')
+    pdf.set_font("Sans", "", 16)
+    pdf.cell(0, baze_point + 60, 'за участие в XII международном форуме', new_x="LMARGIN", align='C')
+    pdf.cell(0, baze_point + 75, '"ОБРАЗОВАНИЕ: РЕАЛИИ И ПЕРСПЕКТИВЫ"', new_x="LMARGIN", align='C')
 
-#     pdf.set_font("Sans", "", 14)
-#     pdf.cell(0, 500, code, new_x="LMARGIN", new_y="NEXT", align='C')
+    pdf.set_font("Sans", "", 14)
+    pdf.cell(0, 500, code, new_x="LMARGIN", new_y="NEXT", align='C')
     
-#     pdf.set_display_mode(zoom='fullpage', layout='continuous')
+    pdf.set_display_mode(zoom='fullpage', layout='continuous')
 
-#     pdf.output("To_Send/" + full_name + ".pdf")
+    pdf.output("To_Send/" + full_name + ".pdf")
 
-sender = "msboriskv@gmail.com"
-password = os.getenv("point")
-msg = MIMEMultipart()
-msg["From"] = sender
-msg["To"] = sender
-msg["Subject"] = 'Сертификат форума "Образование: Реалии и перспективы"'
+    sender = "msboriskv@gmail.com"
+    password = os.getenv("point")
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = sender
+    msg["Subject"] = 'Сертификат форума "Образование: Реалии и перспективы"'
 
-msg.attach(MIMEText("Здравствуйте, отправляем Вам сертификат форума - Образование: Реалии и перспективы"))
+    msg.attach(MIMEText("Здравствуйте, отправляем Вам сертификат форума - Образование: Реалии и перспективы"))
 
-for file in os.listdir("To_Send"):
-    filename = os.path.basename(file)
-    ftype, encoding = mimetypes.guess_type(file)
-    file_type, subtype = ftype.split("/")
-    print(file_type, subtype)
+    for file in os.listdir("To_Send"):
+        filename = os.path.basename(file)
+        ftype, encoding = mimetypes.guess_type(file)
+        file_type, subtype = ftype.split("/")
+        print(file_type, subtype)
     
-    if file_type == "text":
-        with open(f"To_Send/{file}") as f:
-            file = MIMEText(f.read())
-    elif file_type == "image":
-        with open(f"To_Send/{file}", "rb") as f:
-            file = MIMEImage(f.read(), subtype)
-    elif file_type == "audio":
-        with open(f"To_Send/{file}", "rb") as f:
-            file = MIMEAudio(f.read(), subtype)
-    elif file_type == "application":
-        with open(f"To_Send/{file}", "rb") as f:
-            file = MIMEApplication(f.read(), subtype)
-    else:
-        continue
+        if file_type == "text":
+            with open(f"To_Send/{file}") as f:
+                file = MIMEText(f.read())
+        elif file_type == "image":
+            with open(f"To_Send/{file}", "rb") as f:
+                file = MIMEImage(f.read(), subtype)
+        elif file_type == "audio":
+            with open(f"To_Send/{file}", "rb") as f:
+                file = MIMEAudio(f.read(), subtype)
+        elif file_type == "application":
+            with open(f"To_Send/{file}", "rb") as f:
+                file = MIMEApplication(f.read(), subtype)
+        else:
+            continue
     
-    #file.add_header('content-desposition, 'To_Send', filename = '123.pdf')
-    msg.attach(file)
+        #file.add_header('content-desposition, 'To_Send', filename = '123.pdf')
+        msg.attach(file)
 
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
 
-server.login(sender, password)
+    server.login(sender, password)
 
-server.sendmail(sender, sender, msg.as_string())
+    server.sendmail(sender, mail, msg.as_string())
+    shutil.rmtree('To_Send')
